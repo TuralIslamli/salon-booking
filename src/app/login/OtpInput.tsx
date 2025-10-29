c5c5bfbf3b13e190e5f515babc144f074268e545
islamliti <tural.islamli@kapitalbank.az>
2025-10-29 15:25:04 +0400

    error tost

--- src/app/login/OtpInput.tsx
+++ src/app/login/OtpInput.tsx
@@ -1,11 +1,12 @@
 import { Button } from 'primereact/button';
 import { InputOtp } from 'primereact/inputotp';
-import { Dispatch, SetStateAction, useState } from 'react';
+import { Dispatch, SetStateAction, useRef, useState } from 'react';
 import styles from './index.module.scss';
 import { normalizePhone } from '../utils';
 import { useMutation } from '@tanstack/react-query';
 import { useRouter } from 'next/navigation';
 import { postOtp } from '../api';
+import { Toast } from 'primereact/toast';
 
 interface IProps {
   setStep: Dispatch<SetStateAction<'phone' | 'otp'>>;
@@ -15,16 +16,23 @@ interface IProps {
 function OtpInput({ setStep, phone }: IProps) {
   const [otp, setOtp] = useState<string>('');
   const router = useRouter();
+  const toast = useRef<Toast>(null);
 
   const { mutate: otpMutate } = useMutation({
     mutationKey: ['otp'],
     mutationFn: async () =>
       postOtp({ phone: normalizePhone(phone), code: otp }),
     onSuccess: (data: any) => {
-      localStorage.setItem("customerToken", data.data.token.access_token);
+      localStorage.setItem('customerToken', data.data.token.access_token);
       router.push('/');
     },
     onError: (error: any) => {
+      toast.current?.show({
+        severity: 'error',
+        summary: 'Error',
+        detail: error?.response?.data?.message,
+        life: 3000,
+      });
       console.error('Error during otp:', error);
     },
   });
@@ -35,6 +43,7 @@ function OtpInput({ setStep, phone }: IProps) {
 
   return (
     <div className={styles.otpInput}>
+      <Toast ref={toast} />
       <InputOtp
         value={otp}
         onChange={(e) => setOtp(e.value as string)}
@@ -42,8 +51,12 @@ function OtpInput({ setStep, phone }: IProps) {
         style={{ gap: 10 }}
       />
       <div>
-        <Button label="Yenidən göndər" link></Button>
-        <Button label="Təsdiqlə" onClick={handleVerifyOtp}></Button>
+        {/* <Button label="Yenidən göndər" link></Button> */}
+        <Button
+          label="Təsdiqlə"
+          disabled={otp.length < 4}
+          onClick={handleVerifyOtp}
+        ></Button>
       </div>
     </div>
   );
